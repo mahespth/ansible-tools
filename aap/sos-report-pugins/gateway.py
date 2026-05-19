1c8013b1d05dbb9c4f932d57a23c8f9f7ca86cc3
Steve Maher <steve@m4her.com>
2026-05-19 10:57:41 +0100

    Add sos plugin for Ansible Automation Platform gateway
    Implement sos plugin for Ansible Automation Platform gateway to collect relevant information.

---
+++ aap/sos-report-pugins/gateway.py
@@ -0,0 +1,57 @@
+# sos plugin for the gateway for rh issues as non exist in 2.5
+# ------------------------------------------------------------------
+
+try:
+    from sos.plugins import Plugin, RedHatPlugin
+except ImportError:
+    from sos.report.plugins import Plugin, RedHatPlugin
+
+SOSREPORT_CONTROLLER_COMMANDS = [
+    "aap-gateway-manage --version",  # gateway version
+    "aap-gateway-manage list_services",  # gateway cluster configuration
+    "tree -d /etc/ansible-automation-platform",  # show me the dirs
+    "umask -p",  # check current umask
+]
+
+
+SOSREPORT_CONTROLLER_DIRS = [
+    "/var/log/ansible-automation-platform/gateway/",
+    "/etc/ansible-automation-platform/gateway/",
+    "/etc/supervisord.conf",
+    "/etc/supervisord.d/",
+    "/etc/nginx/",
+    "/var/log/supervisor",
+    "/var/log/redis",
+    "/var/log/dist-upgrade",
+    "/var/log/installer",
+    "/var/log/unattended-upgrades",
+]
+
+SOSREPORT_FORBIDDEN_PATHS = [
+    "/etc/ansible-automation-platform/gateway/SECRET_KEY",
+    "/etc/ansible-automation-platform/gateway/cache.cert",
+    "/etc/ansible-automation-platform/gateway/cache.key"
+]
+
+
+class Controller(Plugin, RedHatPlugin):
+    '''Collect Ansible Automation Platform gateway information'''
+
+    plugin_name = "controller"
+    short_desc = "Ansible Automation Platform gateway information"
+
+    def setup(self):
+        for path in SOSREPORT_CONTROLLER_DIRS:
+            self.add_copy_spec(path)
+
+        for path in SOSREPORT_FORBIDDEN_PATHS:
+            self.add_forbidden_path(path)
+
+        self.add_cmd_output(SOSREPORT_CONTROLLER_COMMANDS)
+
+    def postproc(self):
+
+        # remove password
+        jreg = r"(PASSWORD\s*=)\'(.+)\'"
+        repl = r"\1********"
+        self.do_path_regex_sub("/etc/ansible-automation-platform/gateway/settings.py", jreg, repl)
